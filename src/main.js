e443130032ba61100082316ab0321c4467d4c6d4
LucianViana <luciano.souzaviana@gmail.com>
2023-11-24 15:22:10 -0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -9,40 +9,20 @@ import directives from './directives';
 import filters from './filters';
 import './registerServiceWorker'
 import {Auth} from '@aws-amplify/auth'
-//Amplify.configure({
-//   Auth: {
-//     region: process.env.VUE_APP_AUTH_REGION,
-//     userPoolId: process.env.VUE_APP_AUTH_USER_POOL_ID,
-//     userPoolWebClientId: process.env.VUE_APP_AUTH_USER_POOL_WEB_CLIENT_ID,
-//     mandatorySignIn: true
-//   }
-// })
 
-// const myAppConfig = {
-//   'aws_appsync_graphqlEndpoint': process.env.VUE_APP_AWS_APPSYNC_GRAPHQL_ENDPOINT,
-//   'aws_appsync_region': process.env.VUE_APP_AWS_APPSYNC_REGION,
-//   'aws_appsync_authenticationType': process.env.VUE_APP_AWS_APPSYNC_AUTHENTICATION_TYPE
-// }
 Amplify.configure({
   Auth: {
-    // region: process.env.VUE_APP_AUTH_REGION,
-    // userPoolId: process.env.VUE_APP_AUTH_USER_POOL_ID,
-    // userPoolWebClientId: process.env.VUE_APP_AUTH_USER_POOL_WEB_CLIENT_ID,
-    // mandatorySignIn: true
-    region: 'eu-west-1',
-    userPoolId: 'eu-west-1_QS1c6ekh9',
-    userPoolWebClientId: '74v6ncblddu2of1jf6alhacvpl',
-    mandatorySignIn: true
+     region: process.env.VUE_APP_AUTH_REGION,
+     userPoolId: process.env.VUE_APP_AUTH_USER_POOL_ID,
+     userPoolWebClientId: process.env.VUE_APP_AUTH_USER_POOL_WEB_CLIENT_ID,
+     mandatorySignIn: true
   }
 })
 
 const myAppConfig = {
-  //'aws_appsync_graphqlEndpoint': process.env.VUE_APP_AWS_APPSYNC_GRAPHQL_ENDPOINT,
-  'aws_appsync_graphqlEndpoint': "https://7y4ic5xpcfczjj3mys4arlp2gi.appsync-api.eu-west-1.amazonaws.com/graphql",
-  //'aws_appsync_region': process.env.VUE_APP_AWS_APPSYNC_REGION,
-  'aws_appsync_region': "eu-west-1",
-  //'aws_appsync_authenticationType': process.env.VUE_APP_AWS_APPSYNC_AUTHENTICATION_TYPE
-  'aws_appsync_authenticationType': 'AMAZON_COGNITO_USER_POOLS'
+  'aws_appsync_graphqlEndpoint': process.env.VUE_APP_AWS_APPSYNC_GRAPHQL_ENDPOINT,
+  'aws_appsync_region': process.env.VUE_APP_AWS_APPSYNC_REGION,
+  'aws_appsync_authenticationType': process.env.VUE_APP_AWS_APPSYNC_AUTHENTICATION_TYPE
 }
 Amplify.configure(myAppConfig);
 Auth.configure(myAppConfig);
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,6 @@ module.exports = {
     container: {
       center: true
     },
-    maxHeight: {
-      'full': '85%',
-    },
     extend: {
       fontFamily: {
         sans: [...defaultTheme.fontFamily.sans]
@@ -23,22 +20,6 @@ module.exports = {
         'lighter': '#E1E8ED',
         'lightest': '#F5F8FA',
       }
-    },
-    screens: {
-      'sm': '360px',
-      // => @media (min-width: 360px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
     },
   },
   variants: {
